1e500eab5e7d18e8592422f9d64fa3544395075b
Lyydtc <84257922+Lyydtc@users.noreply.github.com>
2022-06-05 20:14:01 +0800

    页面文件

--- page_file.py
+++ page_file.py
@@ -4,7 +4,7 @@ import time
 
 if __name__ == "__main__":
     tic = time.time()
-    with open(r".\mySpider\news.csv") as fin1, open(r".\pages.csv", "w", newline='') as fout1:
+    with open(r".\files\news.csv") as fin1, open(r".\files\pages.csv", "w", newline='') as fout1:
         news_reader = csv.reader(fin1)
         news_writer = csv.writer(fout1)
         count = 0
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                 continue
             did += 1
             news_writer.writerow([did, row[0]])
-    with open(r".\mySpider\rmrb.csv", encoding='utf-8') as fin2, open(r".\pages.csv", "a", newline='') as fout2:
+    with open(r".\files\rmrb.csv", encoding='utf-8') as fin2, open(r".\files\pages.csv", "a", newline='') as fout2:
         rmrb_reader = csv.reader(fin2)
         rmrb_writer = csv.writer(fout2)
         count = 0
