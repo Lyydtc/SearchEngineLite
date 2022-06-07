72e2ea6b23828fedf66009d864a6eb95a0da6cc9
Lyydtc <84257922+Lyydtc@users.noreply.github.com>
2022-06-07 21:38:32 +0800

    Update trie.py

--- trie.py
+++ trie.py
@@ -2,22 +2,24 @@ class TrieNode:
     def __init__(self):
         self.child = {}
         self.end = False
+        self.tid = 0
 
-    def insert(self, word: str):
+    def insert(self, word: str, term_id=0):
         cur = self
         for char in word:
             if char not in cur.child:
                 cur.child[char] = TrieNode()
             cur = cur.child[char]
         cur.end = True
+        cur.tid = term_id
 
     def search(self, word: str):
         cur = self
         for char in word:
             if char not in cur.child:
-                return False
+                return False, 0
             cur = cur.child[char]
-        return cur.end
+        return cur.end, cur.tid
 
 
 if __name__ == "__main__":
