34918ff722949dbbae669c70dcd572e887471926
Lyydtc <84257922+Lyydtc@users.noreply.github.com>
2022-06-07 17:07:04 +0800

    中文分词

--- cn_segment.py
+++ cn_segment.py
@@ -1,44 +1,55 @@
+#coding=utf-8
 import csv
-from trie import TrieNode
+import re
 
 
 def forward_segment(text, dic):
     word_list = []
-    i = 0
-    len_text = len(text)
-    while i < len_text:
-        longest_word = text[i]                      # 当前扫描位置的单字
-        for j in range(i + 1, len_text + 1):       # 所有可能的结尾
-            word = text[i:j]                        # 从当前位置到结尾的连续字符串
-            if word in dic:                         # 在词典中
-                if len(word) > len(longest_word):   # 并且更长
-                    longest_word = word             # 则更优先输出
-        word_list.append(longest_word)              # 输出最长词
-        i += len(longest_word)                      # 正向扫描
+    sen_list = re.split('[，。、：；“”？！—…（）《》]', text)
+    for sen in sen_list:
+        i = 0
+        while i < len(sen):
+            longest_word = sen[i]                      # 当前扫描位置的单字
+            for j in range(i + 1, len(sen) + 1):       # 所有可能的结尾
+                word = sen[i:j]                        # 从当前位置到结尾的连续字符串
+                if word in dic:                         # 在词典中
+                    if len(word) > len(longest_word):   # 并且更长
+                        longest_word = word             # 则更优先输出
+            word_list.append(longest_word)              # 输出最长词
+            i += len(longest_word)                      # 正向扫描
     return word_list
 
 
 def backward_segment(text, dic):
-    word_list = []
-    i = len(text) - 1
-    while i >= 0:                                   # 扫描位置作为终点
-        longest_word = text[i]                      # 扫描位置的单字
-        for j in range(0, i):                       # 遍历[0, i]区间作为待查询词语的起点
-            word = text[j: i + 1]                   # 取出[j, i]区间作为待查询单词
-            if word in dic:
-                if len(word) > len(longest_word):   # 越长优先级越高
-                    longest_word = word
-                    break
-        word_list.insert(0, longest_word)           # 逆向扫描，所以越先查出的单词在位置上越靠后
-        i -= len(longest_word)
-    return word_list
+    text_word_list = []
+    sen_list = re.split('[，。、：；“”？！—…（）《》]', text)
+    for sen in sen_list:
+        i = len(sen) - 1
+        sen_word_list = []
+        while i >= 0:                                   # 扫描位置作为终点
+            longest_word = sen[i]                      # 扫描位置的单字
+            for j in range(0, i):                       # 遍历[0, i]区间作为待查询词语的起点
+                word = sen[j: i + 1]                   # 取出[j, i]区间作为待查询单词
+                if word in dic:
+                    if len(word) > len(longest_word):   # 越长优先级越高
+                        longest_word = word
+                        break
+            sen_word_list.insert(0, longest_word)           # 逆向扫描，所以越先查出的单词在位置上越靠后
+            i -= len(longest_word)
+        text_word_list += sen_word_list
+    return text_word_list
 
 
 def count_single_char(word_list: list):  # 统计单字成词的个数
     return sum(1 for word in word_list if len(word) == 1)
 
 
-def bi_segment(text, dic):
+def bi_segment(text):
+    dic = []
+    with open(r".\files\dict.csv", encoding='utf-8') as cn_dict_csv:
+        cn_dict = csv.reader(cn_dict_csv)
+        for word in cn_dict:
+            dic += word
     f = forward_segment(text, dic)
     b = backward_segment(text, dic)
     if len(f) < len(b):                                  # 词数更少优先级更高
@@ -53,9 +64,4 @@ def bi_segment(text, dic):
 
 
 if __name__ == '__main__':
-    dic = []
-    with open(r".\files\dict.csv", encoding='utf-8') as cn_dict_csv:
-        cn_dict = csv.reader(cn_dict_csv)
-        for word in cn_dict:
-            dic += word
-    print(bi_segment('中国人民解放军', dic))
+    print(bi_segment(''))
